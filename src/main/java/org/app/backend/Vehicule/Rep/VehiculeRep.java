eea79d549bbd10cc7e4a77168af67f64107baed7
Oussama-benrkia <oussamabenrkia0@gmail.com>
2024-04-19 15:52:51 +0100

    add search to vehi

--- src/main/java/org/app/backend/Vehicule/Rep/VehiculeRep.java
+++ src/main/java/org/app/backend/Vehicule/Rep/VehiculeRep.java
@@ -1,6 +1,8 @@
 package org.app.backend.Vehicule.Rep;
 
 import org.app.backend.Vehicule.Model.Vehicule;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +13,12 @@ public interface VehiculeRep extends JpaRepository<Vehicule, Long>{
 
     Vehicule findByMatricule(String matricule);
     List<Vehicule> findByMatriculeAndModele(String matricule, String modele);
+    List<Vehicule>findAllByNomContainingOrMatriculeContainingOrModeleContaining(String re,String r,String m);
+
+
+    Page<Vehicule>findAllByNomContainingOrMatriculeContainingOrModeleContaining(String re, String r, String m,Pageable p);
+
+
 
 
 }
